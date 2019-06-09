afe8355cb17805dd46db00aaced42ed952157f4c
linzhihua <414676265@qq.com>
2019-06-09 11:03:23 +0800

    First commit

---
+++ app/src/main/java/com/process/mytest/MainActivity.java
@@ -0,0 +1,50 @@
+package com.process.mytest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.process.mytest.activity.GitTestActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /**
+     * Hello World!
+     */
+    private Button mBt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mBt = (Button) findViewById(R.id.bt);
+        mBt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            case R.id.bt:
+                Toast.makeText(this, "skip onclick...", Toast.LENGTH_SHORT).show();
+                toSkip(GitTestActivity.class);
+                break;
+        }
+    }
+
+    private void toSkip(Class<?> activity) {
+        if (null != activity) {
+            Intent intent = new Intent(this, activity);
+            startActivity(intent);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/process/mytest/activity/GitTestActivity.java
@@ -0,0 +1,23 @@
+package com.process.mytest.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.process.mytest.R;
+
+public class GitTestActivity extends AppCompatActivity {
+    public static final String TAG=GitTestActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_test);
+    }
+
+    private void test1(){
+        Log.d(TAG, "test1: ");
+    }
+
+
+}
